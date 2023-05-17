b94311c13356e17cd87ab4c9921b370a44ad42ee
Sangeeth R <47264999+DRIRU@users.noreply.github.com>
2023-05-17 11:50:30 +0300

    Update to info.py
    added server info command

--- cogs/info.py
+++ cogs/info.py
@@ -2,7 +2,7 @@ import discord
 import datetime
 from discord.ext import commands
 from discord import app_commands
-class User(commands.Cog):
+class Info(commands.Cog):
     def __init__(self, client):
         self.client = client
     @commands.Cog.listener()
@@ -18,7 +18,7 @@ class User(commands.Cog):
         embed.add_field(name = "Nick Name", value = user.display_name)
         await ctx.send(embed = embed)
     @app_commands.command(name = "uinfo", description="shows information")
-    async def uinfo(self, interaction: discord.Interaction, user: discord.Member= None):
+    async def userInfo(self, interaction: discord.Interaction, user: discord.Member= None):
         if user == None:
             user = interaction.user
         embed = discord.Embed(title = "User Information", description = f"Information of {user.mention}", timestamp=datetime.datetime.now())
@@ -29,7 +29,18 @@ class User(commands.Cog):
         embed.add_field(name = "Created on", value = user.created_at.strftime("%A %d %b %Y, %I:%M %p"))
         embed.add_field(name = "Joined on", value = user.joined_at.strftime("%A %d %b %Y, %I:%M %p"))
         await interaction.response.send_message(embed = embed)
-    
+
+    @app_commands.command(name = "serverinfo", description="shows server information")
+    async def serverInfo(self, interaction: discord.Interaction):
+        embed = discord.Embed(title = "Server Information", description = f"Information of {interaction.guild.name}", timestamp=datetime.datetime.now())
+        embed.set_thumbnail(url = interaction.guild.icon)
+        embed.add_field(name = "Id", value = interaction.guild.id)
+        embed.add_field(name = "Name", value = interaction.guild.name)
+        embed.add_field(name = "Owner", value = interaction.guild.owner.mention)
+        embed.add_field(name = "Members", value = len(interaction.guild.members))
+        embed.add_field(name = "Channels", value = f"{len(interaction.guild.text_channels)} Text | {len(interaction.guild.voice_channels)} Voice")
+        embed.add_field(name = "Created on", value = interaction.guild.created_at.strftime("%A %d %b %Y, %I:%M %p"))
+        await interaction.response.send_message(embed = embed)
 
 async def setup(client: commands.Bot):
-    await  client.add_cog(User(client))
+    await  client.add_cog(Info(client))
